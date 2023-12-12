5f2c01f4cb70b39fffed595b641cf6e50fc60747
BuathierTom <buathier_tom@orange.fr>
2023-12-12 01:20:25 +0100

    Add: Representation method for the face class

--- src/face.py
+++ src/face.py
@@ -1,3 +1,7 @@
 class Face: 
     def __init__(self, s1, s2, s3):
         self.sommets = [s1, s2, s3]
+        
+    
+    def __repr__(self):
+        return f"Face({self.sommets[0]}, {self.sommets[1]}, {self.sommets[2]})"
