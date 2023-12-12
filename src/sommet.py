711f1e966abe5d795d6e41ef9fa0ec350efc60ce
BuathierTom <buathier_tom@orange.fr>
2023-12-12 01:21:41 +0100

    Add: New representation // method for the translation and homothety

--- src/sommet.py
+++ src/sommet.py
@@ -1,9 +1,25 @@
 class Sommet:
-    def __init__(self, x, y, z):
+    def __init__(self, x: int, y: int, z: int):
         self.x = x
         self.y = y
         self.z = z
         self.voisins = []
         
-    def ajouter_voisin(self, voisin):
+    def setVoisins(self, voisin: list):
         self.voisins.append(voisin)
+        
+    def getVoisins(self):
+        return self.voisins
+    
+    def translater(self, translation_x, translation_y, translation_z):
+        self.x += translation_x
+        self.y += translation_y
+        self.z += translation_z
+        
+    def homothetie(self, facteur_echelle):
+        self.x *= facteur_echelle
+        self.y *= facteur_echelle
+        self.z *= facteur_echelle
+    
+    def __repr__(self):
+        return f"Sommet({self.x}, {self.y}, {self.z})"
